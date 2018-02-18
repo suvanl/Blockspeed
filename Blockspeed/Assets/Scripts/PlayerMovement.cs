9ab28688bfe24753bf5d87ec843c424e1be96ead
Suvan <mr.slrmx@gmail.com>
2018-02-18 19:23:52 +0000

    Add project files
    The "Builds", "etc", "Library" and "obj" directories have not been
    committed. See `.gitignore` for more information on which other files
    would've been ignored during the commit.

---
+++ Blockspeed/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // This is a reference to the Rigidbody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+	// Marked as "FixedUpdate" rather than just "Update"
+    // as it is being used for physics stuff
+	void FixedUpdate ()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") ) // If the player is pressing the "D" key
+        {
+            // Add a force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a")) // If the player is pressing the "A" key
+        {
+            // Add a force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+}
